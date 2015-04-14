e7dc9ae2e37867703de2db49ceef46f8f2912234
oilbeater <liumengxinfly@gmail.com>
2015-04-14 11:06:21 +0800

    update

--- downloader.py
+++ downloader.py
@@ -25,20 +25,22 @@ def main():
         print "Already download"
         exit()
 
-    base_url = cf.get("esl", "download_base")
+    base_url = cf.get("esl", "download_base_url")
     pod_base_url = base_url + cf.get("esl", "pod_pre")
     cafe_base_url = base_url + cf.get("esl", "cafe_pre")
     suffix = cf.get("esl", "suffix")
 
-    for pod in range(latest_pod):
-        url = pod_base_url + pod + suffix
-        code = os.system("timeout 600 axel -n 5" + url)
+    esl = [1079,1089,1106,112,113,118,127,130,294,417,61,62,63,64,65,719,831,87,997]
+    for pod in esl:
+        url = pod_base_url + str(pod) + suffix
+        print "download " + url
+        code = os.system("timeout 500 axel -n 10 " + url)
         # if code == '256':
         #     break
-
-    for cafe in range(latest_cafe):
-        url = cafe_base_url + cafe + suffix
-        code = os.system("timeout 600 axel -n 5" + url)
+    ec = [117,60,66,71,88,96]
+    for cafe in ec:
+        url = cafe_base_url + str(cafe) + suffix
+        code = os.system("timeout 500 axel -n 10 " + url)
         # if code == '256':
         #     break
 
